82421dbf0a2e992f96b75a3c9b4b27182b991be6
AyamotoKohei <koh14taka2@gmail.com>
2021-12-07 23:02:51 +0900

    add: 複数のGenericsの学習

---
+++ 6-1-3/example.ts
@@ -0,0 +1,12 @@
+function pick<T, K extends keyof T>(props: T, key: K) {
+  return props[key];
+}
+const obj = {
+  name: "Taro",
+  amount: 0,
+  flag: false,
+};
+const value1 = pick(obj, "name");
+const value2 = pick(obj, "amount");
+const value3 = pick(obj, "flag");
+const value4 = pick(obj, "test"); // Error
